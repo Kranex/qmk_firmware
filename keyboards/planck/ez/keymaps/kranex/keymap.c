43cffa17b06670f01fc7c2b4e5f6beedc277e5f7
Kranex <oliverstrik@gmail.com>
2020-02-11 10:23:43 +0100

    moved pageup, pagedown

--- keyboards/planck/ez/keymaps/kranex/keymap.c
+++ keyboards/planck/ez/keymaps/kranex/keymap.c
@@ -57,16 +57,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_LCTL ,KC_LGUI ,KC_NO   ,KC_LALT ,LOWER   ,KC_SPC  ,KC_NO   ,RAISE   ,KC_LEFT ,KC_DOWN ,KC_UP   ,KC_RGHT ),
 
   [_LOWER] = LAYOUT_planck_grid(
-      KC_TILD ,KC_EXLM ,KC_AT   ,KC_HASH ,KC_DLR  ,KC_PERC ,KC_CIRC ,KC_AMPR ,KC_ASTR ,KC_LPRN ,KC_RPRN ,_______ ,
+      KC_TILD ,KC_EXLM ,KC_AT   ,KC_HASH ,KC_DLR  ,KC_PERC ,KC_CIRC ,KC_AMPR ,KC_ASTR ,KC_LPRN ,KC_RPRN ,KC_DEL  ,
       KC_DEL  ,KC_F1   ,KC_F2   ,KC_F3   ,KC_F4   ,KC_F5   ,KC_F6   ,KC_UNDS ,KC_PLUS ,KC_LCBR ,KC_RCBR ,KC_PIPE ,
       _______ ,KC_F7   ,KC_F8   ,KC_F9   ,KC_F10  ,KC_F11  ,KC_F12  ,_______ ,_______ ,KC_HOME ,KC_END  ,_______ ,
       _______ ,_______ ,_______ ,_______ ,_______ ,_______ ,KC_NO   ,_______ ,KC_MNXT ,KC_VOLD ,KC_VOLU ,KC_MPLY ),
 
   [_RAISE] = LAYOUT_planck_grid(
-      KC_GRV  ,KC_1    ,KC_2    ,KC_3    ,KC_4    ,KC_5    ,KC_6    ,KC_7    ,KC_8    ,KC_9    ,KC_0    ,_______ ,
+      KC_GRV  ,KC_1    ,KC_2    ,KC_3    ,KC_4    ,KC_5    ,KC_6    ,KC_7    ,KC_8    ,KC_9    ,KC_0    ,KC_DEL ,
       KC_DEL  ,KC_F1   ,KC_F2   ,KC_F3   ,KC_F4   ,KC_F5   ,KC_F6   ,KC_MINS ,KC_EQL  ,KC_LBRC ,KC_RBRC ,KC_BSLS ,
-      _______ ,KC_F7   ,KC_F8   ,KC_F9   ,KC_F10  ,KC_F11  ,KC_F12  ,_______ ,_______ ,KC_PGUP ,KC_PGDN ,_______ ,
-      _______ ,_______ ,_______ ,_______ ,_______ ,_______ ,KC_NO   ,_______ ,KC_MNXT ,KC_VOLD ,KC_VOLU ,KC_MPLY ),
+      _______ ,KC_F7   ,KC_F8   ,KC_F9   ,KC_F10  ,KC_F11  ,KC_F12  ,_______ ,_______ ,_______ ,_______ ,_______ ,
+      _______ ,_______ ,_______ ,_______ ,_______ ,_______ ,KC_NO   ,_______ ,_______ ,KC_PGUP ,KC_PGDN ,_______ ),
 
   [_ADJUST] = LAYOUT_planck_grid(
       _______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,
@@ -101,19 +101,19 @@ const uint8_t PROGMEM ledmap[][DRIVER_LED_TOTAL][3] = {
     [_LOWER] = {
       COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_0 ,
       COLOR_1 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,
-      COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,
+      COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_1 ,COLOR_1 ,COLOR_0 ,
       COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_1 ,COLOR_0          ,COLOR_0 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 },
 
     [_RAISE] = {
       COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_0 ,
       COLOR_1 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,
       COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,
-      COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0          ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 },
+      COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0          ,COLOR_0 ,COLOR_0 ,COLOR_1 ,COLOR_1 ,COLOR_0 },
 
     [_ADJUST] = {
       COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,
-      COLOR_1 ,COLOR_0 ,COLOR_0 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_0 ,COLOR_1 ,
-      COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_0 ,COLOR_0 ,
+      COLOR_1 ,COLOR_0 ,COLOR_0 ,COLOR_1 ,COLOR_1 ,COLOR_0 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_0 ,COLOR_1 ,
+      COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_0 ,COLOR_0 ,
       COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_1 ,COLOR_0          ,COLOR_1 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 },
 
     [_FUNC] = {
