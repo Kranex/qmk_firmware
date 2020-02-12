2a4e160938a3ff7596284689f1f77ae020264676
Kranex <oliverstrik@gmail.com>
2020-02-12 16:05:29 +0100

    added mouse enable rule

--- keyboards/planck/ez/keymaps/kranex/keymap.c
+++ keyboards/planck/ez/keymaps/kranex/keymap.c
@@ -47,6 +47,7 @@ enum planck_layers {
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
 #define FN_TAB LT(_FUNC, KC_TAB)
+#define BSPCDEL LT(KC_DEL, KC_BSPC)
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -57,13 +58,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_LCTL ,KC_LGUI ,KC_NO   ,KC_LALT ,LOWER   ,KC_SPC  ,KC_NO   ,RAISE   ,KC_LEFT ,KC_DOWN ,KC_UP   ,KC_RGHT ),
 
   [_LOWER] = LAYOUT_planck_grid(
-      KC_TILD ,KC_EXLM ,KC_AT   ,KC_HASH ,KC_DLR  ,KC_PERC ,KC_CIRC ,KC_AMPR ,KC_ASTR ,KC_LPRN ,KC_RPRN ,KC_DEL  ,
+      KC_TILD ,KC_EXLM ,KC_AT   ,KC_HASH ,KC_DLR  ,KC_PERC ,KC_CIRC ,KC_AMPR ,KC_ASTR ,KC_LPRN ,KC_RPRN ,_______ ,
       KC_DEL  ,KC_F1   ,KC_F2   ,KC_F3   ,KC_F4   ,KC_F5   ,KC_F6   ,KC_UNDS ,KC_PLUS ,KC_LCBR ,KC_RCBR ,KC_PIPE ,
       _______ ,KC_F7   ,KC_F8   ,KC_F9   ,KC_F10  ,KC_F11  ,KC_F12  ,_______ ,_______ ,KC_HOME ,KC_END  ,_______ ,
       _______ ,_______ ,_______ ,_______ ,_______ ,_______ ,KC_NO   ,_______ ,KC_MNXT ,KC_VOLD ,KC_VOLU ,KC_MPLY ),
 
   [_RAISE] = LAYOUT_planck_grid(
-      KC_GRV  ,KC_1    ,KC_2    ,KC_3    ,KC_4    ,KC_5    ,KC_6    ,KC_7    ,KC_8    ,KC_9    ,KC_0    ,KC_DEL ,
+      KC_GRV  ,KC_1    ,KC_2    ,KC_3    ,KC_4    ,KC_5    ,KC_6    ,KC_7    ,KC_8    ,KC_9    ,KC_0    ,_______ ,
       KC_DEL  ,KC_F1   ,KC_F2   ,KC_F3   ,KC_F4   ,KC_F5   ,KC_F6   ,KC_MINS ,KC_EQL  ,KC_LBRC ,KC_RBRC ,KC_BSLS ,
       _______ ,KC_F7   ,KC_F8   ,KC_F9   ,KC_F10  ,KC_F11  ,KC_F12  ,_______ ,_______ ,_______ ,_______ ,_______ ,
       _______ ,_______ ,_______ ,_______ ,_______ ,_______ ,KC_NO   ,_______ ,_______ ,KC_PGUP ,KC_PGDN ,_______ ),
@@ -75,8 +76,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______ ,_______ ,_______ ,_______ ,_______ ,_______ ,KC_NO   ,_______ ,_______ ,_______ ,_______ ,_______ ),
 
   [_FUNC] = LAYOUT_planck_grid(
-      _______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,KC_PSCR ,_______ ,
-      _______ ,_______ ,_______ ,_______ ,_______ ,_______ ,KC_LEFT ,KC_DOWN ,KC_UP   ,KC_RGHT ,_______ ,_______ ,
+      _______ ,KC_BTN2 ,KC_MS_U ,KC_BTN1 ,KC_WH_U ,_______ ,_______ ,_______ ,_______ ,_______ ,KC_PSCR ,_______ ,
+      _______ ,KC_MS_L ,KC_MS_D ,KC_MS_R ,KC_WH_D ,_______ ,KC_LEFT ,KC_DOWN ,KC_UP   ,KC_RGHT ,_______ ,_______ ,
       _______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,
       _______ ,_______ ,_______ ,_______ ,_______ ,_______ ,KC_NO   ,_______ ,_______ ,_______ ,_______ ,_______ ),
 };
@@ -117,11 +118,17 @@ const uint8_t PROGMEM ledmap[][DRIVER_LED_TOTAL][3] = {
       COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_1 ,COLOR_0          ,COLOR_1 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 },
 
     [_FUNC] = {
-      COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_1 ,COLOR_0 ,
-      COLOR_1 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_0 ,COLOR_0 ,
+      COLOR_0 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_1 ,COLOR_0 ,
+      COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_0 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_0 ,COLOR_0 ,
       COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,
       COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0          ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 },
 };
+
+
+/**
+ * Freeky stuff that has nothing to do with me.
+ * vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+ */
 
 void set_layer_color(int layer) {
   for (int i = 0; i < DRIVER_LED_TOTAL; i++) {
