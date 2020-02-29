ad97b6d3c2f90ce779efd0dea9df43cc34382a67
Kranex <oliverstrik@gmail.com>
2020-02-29 01:59:32 +0100

    yoink

--- keyboards/planck/ez/keymaps/kranex/keymap.c
+++ keyboards/planck/ez/keymaps/kranex/keymap.c
@@ -67,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_GRV  ,KC_1    ,KC_2    ,KC_3    ,KC_4    ,KC_5    ,KC_6    ,KC_7    ,KC_8    ,KC_9    ,KC_0    ,_______ ,
       KC_DEL  ,KC_F1   ,KC_F2   ,KC_F3   ,KC_F4   ,KC_F5   ,KC_F6   ,KC_MINS ,KC_EQL  ,KC_LBRC ,KC_RBRC ,KC_BSLS ,
       _______ ,KC_F7   ,KC_F8   ,KC_F9   ,KC_F10  ,KC_F11  ,KC_F12  ,_______ ,_______ ,_______ ,_______ ,_______ ,
-      _______ ,_______ ,_______ ,_______ ,_______ ,_______ ,KC_NO   ,_______ ,_______ ,KC_PGUP ,KC_PGDN ,_______ ),
+      _______ ,_______ ,_______ ,_______ ,_______ ,_______ ,KC_NO   ,_______ ,_______ ,KC_PGDN ,KC_PGUP ,_______ ),
 
   [_ADJUST] = LAYOUT_planck_grid(
       _______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,
@@ -76,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______ ,_______ ,_______ ,_______ ,_______ ,_______ ,KC_NO   ,_______ ,_______ ,_______ ,_______ ,_______ ),
 
   [_FUNC] = LAYOUT_planck_grid(
-      _______ ,KC_BTN2 ,KC_MS_U ,KC_BTN1 ,KC_WH_U ,_______ ,_______ ,_______ ,_______ ,_______ ,KC_PSCR ,_______ ,
+      _______ ,KC_BTN2 ,KC_MS_U ,KC_BTN1 ,KC_WH_U ,_______ ,_______ ,_______ ,_______ ,_______ ,KC_PSCR ,KC_DEL  ,
       _______ ,KC_MS_L ,KC_MS_D ,KC_MS_R ,KC_WH_D ,_______ ,KC_LEFT ,KC_DOWN ,KC_UP   ,KC_RGHT ,_______ ,_______ ,
       _______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,
       _______ ,_______ ,_______ ,_______ ,_______ ,_______ ,KC_NO   ,_______ ,_______ ,_______ ,_______ ,_______ ),
@@ -118,7 +118,7 @@ const uint8_t PROGMEM ledmap[][DRIVER_LED_TOTAL][3] = {
       COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_1 ,COLOR_0          ,COLOR_1 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 },
 
     [_FUNC] = {
-      COLOR_0 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_1 ,COLOR_0 ,
+      COLOR_0 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_1 ,COLOR_1 ,
       COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_0 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_1 ,COLOR_0 ,COLOR_0 ,
       COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,
       COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0          ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 ,COLOR_0 },
